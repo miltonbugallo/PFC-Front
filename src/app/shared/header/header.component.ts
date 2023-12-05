74e839db3a7df31381d5613d495cebd854c6e883
Milton Bugallo <mbugallo@hexacta.com>
2023-12-05 11:32:13 -0300

    Commit 33: Agentes y alertas

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,31 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AlertasService } from 'src/app/services/alertas.service';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   
-  constructor( private alertasService: AlertasService){}
+  constructor(private alertasService: AlertasService) {}
 
   alertasEquipos: any[] = [];
   alertasSwitch: any[] = [];
   cantidadAlertasEquipos: number = 0;
   cantidadAlertasSwitchs: number = 0;
 
-
-    obtenerDatos() {
-      this.alertasService.getAlertas().subscribe((datos) => {
-        this.alertasEquipos = datos.equiposObsoletos;
-        this.alertasSwitch = datos.switchSinConexion;
-        this.cantidadAlertasEquipos = this.alertasEquipos.length;
-        this.cantidadAlertasSwitchs = this.alertasSwitch.length;
-      });
-    }
+  obtenerDatos() {
+    this.alertasService.getAlertas().subscribe((datos) => {
+      this.alertasEquipos = datos.equiposObsoletos;
+      this.alertasSwitch = datos.switchSinConexion;
+      this.cantidadAlertasEquipos = this.alertasEquipos.length;
+      this.cantidadAlertasSwitchs = this.alertasSwitch.length;
+    });
+  }
 
   ngOnInit() {
+    // Ejecutar obtenerDatos cada 2 minutos
+    interval(2 * 60 * 1000)  // 2 minutos en milisegundos
+      .subscribe(() => {
+        this.obtenerDatos();
+      });
+    
+    // También ejecutar obtenerDatos al inicio
     this.obtenerDatos();
   }
 }
+
